6043d09ad43cce0b60511de568da7ea506943c79
David Rother <david@edv-drucksysteme.de>
2021-04-17 02:06:39 +0200

    added programmatic interface to retrieve db values with evalpy functions

--- evalpy/__init__.py
+++ evalpy/__init__.py
@@ -11,7 +11,12 @@ forward_run_step = __client.forward_step
 delete_experiment = __client.delete_experiment
 delete_run = __client.delete_run
 commit_run_progress = __client.commit_current_run_progress
+get_run_ids_by_experiment = __client.get_runs_by_experiment_names
+get_run_values = __client.filtered_column_values_run
+get_experiment_names = __client.get_stored_experiment_names
+get_experiment_values = __client.filtered_column_values_experiment
 
 
 __all__ = ["start_run", "end_run", "set_project", "log_run_entries", "log_run_step", "forward_run_step",
-           "delete_experiment", "delete_run", "commit_run_progress"]
+           "delete_experiment", "delete_run", "commit_run_progress", "get_run_ids_by_experiment",
+           "get_run_values", "get_experiment_names", "get_experiment_values"]
